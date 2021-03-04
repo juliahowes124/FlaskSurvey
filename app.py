908dad552a6986736936d1a20d8dc2984e52bb39
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-04 12:15:17 -0800

    finished step 5

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ def index():
     return render_template('survey_start.html', title=title, instructions=instructions)
 
 
+@app.route('/begin', methods=["POST"])
+def begin():
+    return redirect('/questions/0')
+
+
 @app.route('/questions/<int:index>')
 def questions(index):
     question = survey.questions[index]
@@ -27,11 +32,16 @@ def questions(index):
         question=question
     )
 
+
 @app.route('/answer', methods=["POST"])
 def answer():
-    
     responses.append(request.form["answer"])
     index = len(responses)
-    print(responses)
-
+    if index >= len(survey.questions):
+        return redirect('/thankyou')
     return redirect(f"/questions/{index}")
+
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
